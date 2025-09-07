9d1984a4f9cdff960f38f9c9009ad522e9f805ea
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-09-07 12:37:53 +0530

    upd and del done:)

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -47,6 +47,89 @@ const getEmployeeDetails=asyncHandler(
             throw new ApiError(400,err.message)
         }
     }
+);
+
+const removeEmployee=asyncHandler(
+
+    async(req,res)=>{
+        try {
+            const {id}=req.params;
+            const query=`
+            update employee
+            set shop_id=null
+            where pharmacist_id=?
+            `
+            console.log(`removing pharmacist_id${id} from shop...`);
+            
+            const [rows]=await db.execute(query,[id]);
+            return res.status(200).json(new ApiResponse(200,rows,`deleted employee with id=${id}`))
+        } catch (err) {
+            throw new ApiError(400,err.message)
+        }
+
+    }
+)
+const updateEmployeeSalary=asyncHandler(
+
+    async(req,res)=>{
+        try {
+            const {pharmacist_id,newSalary}=req.body;
+            const query=`
+            with cte as (select salary.emp_id as emp_id
+            from salary
+            left join employee on employee.emp_id=salary.emp_id
+            left join pharmacist on employee.pharmacist_id=pharmacist.pharmacist_id
+            where pharmacist.pharmacist_id=?)
+            update salary 
+            set salary=?
+            where emp_id in (select emp_id from cte)
+            `
+            const [rows]=await db.execute(query,[pharmacist_id,newSalary]);
+            return res.status(200).json(new ApiResponse(200,rows,`updated employee salary with id=${pharmacist_id}`))
+        } catch (err) {
+            throw new ApiError(400,err.message)
+        }
+
+    }
+);
+const getAllEmployables=asyncHandler(
+    async(req,res)=>{
+        const query=
+        `select p.pharmacist_id,p.name,p.email,
+        s.salary as prev_salary,
+        CONCAT(
+                TIMESTAMPDIFF(YEAR, p.joined_date, CURRENT_DATE), ' years ',
+                MOD(TIMESTAMPDIFF(MONTH, p.joined_date, CURRENT_DATE), 12), ' months'
+            ) AS experience
+        from employee as e
+        left join pharmacist as p 
+        on e.pharmacist_id=p.pharmacist_id
+        left join salary as s on s.emp_id=e.emp_id
+        where e.shop_id is null
+`
+        const [rows]=await db.execute(query);
+        if(rows.length==0) throw new ApiError(400,"No employables right now!")
+
+    }
+)
+const addEmployee=asyncHandler(
+    async(req,res)=>{
+        const {pharmacist_id}=req.body;
+        if(!pharmacist_id) throw new ApiError(400,"Pls provide id of pharmacist to add")
+        console.log(`adding phamacist_id=${pharmacist_id} in shop_id=${req.shop_id}.....`);
+        const query=`
+            update employee
+            set shop_id=?
+            where pharmacist_id=?
+            `
+            const [rows]=await db.execute(query,[req.shop_id,pharmacist_id]);
+        
+        if(rows.length===0) throw new ApiError(400,"Failed to add employee!")
+        return res.status(200).json(
+            new ApiResponse(200,rows)
+        )
+        
+    }
 )
 
-export {isManager,getEmployeeDetails}
+export {isManager,getEmployeeDetails,removeEmployee,updateEmployeeSalary,addEmployee}
--- src/routes/manager.routes.js
+++ src/routes/manager.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { verifyManagerAccess } from "../middleware/manager_chk.middleware.js";
-import { getEmployeeDetails, isManager } from "../controllers/manager.controller.js";
+import { addEmployee, getEmployeeDetails, isManager, removeEmployee, updateEmployeeSalary } from "../controllers/manager.controller.js";
 
 
 const managerRouter=Router()
@@ -15,5 +15,18 @@ managerRouter.route('/').get(isManager)
 managerRouter
 .route('/getEmployeeDetails')
 .get(getEmployeeDetails)
+
+
+managerRouter
+.route('/removeEmployee/:id')
+.delete(removeEmployee)
+
+managerRouter
+.route("/updateEmployeeSalary")
+.patch(updateEmployeeSalary)
+
+managerRouter
+.route("/addEmployee")
+.put(addEmployee)
 
 export {managerRouter}
